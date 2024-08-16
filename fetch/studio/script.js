6822d45ce2ba76b829e19ca625dd0b9cc70caa5e
nsirtak <nsirtak@gmail.com>
2024-08-15 20:55:30 -0500

    studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,29 @@
 //TODO: Add Your Code Below
+//h3 should be h2
+//img should have alt tag
+
+window.addEventListener("load", function () {
+    fetch(
+      "https://handlers.education.launchcode.org/static/astronauts.json"
+    ).then(function (response) {
+      response.json().then(function (json) {
+        const container = document.getElementById("container");
+        for (let i = 0; i < json.length; i++) {
+          const astronaut = json[i];
+          container.innerHTML += `<div class="astronaut">
+              <div class="bio">
+              <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+              <ul>
+              <li>Hours in space: ${astronaut.hoursInSpace}</li>
+              <li>Active: ${astronaut.active}</li>
+              <li>Skills: ${astronaut.skills}</li>
+              </ul>
+              </div>
+              <img class="avatar" src="${astronaut.picture}">
+              </div>
+          `;
+        }
+      });
+    });
+  });
+  
